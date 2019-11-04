938267e3dcaca3d950dee81d2544d9f33e823feb
JavikusRod <javierrod9191@gmail.com>
2019-11-04 13:22:51 -0500

    updated loading!

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
-import { IonContent, IonPage } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonPage, IonLoading } from '@ionic/react';
+import React, { useState, Fragment } from 'react';
 import NavLayaout from '../components/layout/nav/NavLayout.component';
 import MainLayaout from '../components/layout/main/MainLayaout.component';
 
 const Home: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   return (
     <IonPage>
       <NavLayaout />
-      <IonContent className='ion-padding'>
-        <MainLayaout />
-      </IonContent>
+      {loading ? <IonLoading isOpen={loading} message={'Loading...'} /> : null}
+      <Fragment>
+        <IonContent className='ion-padding'>
+          <MainLayaout />
+        </IonContent>
+      </Fragment>
     </IonPage>
   );
 };
