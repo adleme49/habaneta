9d8e963c16a49f4b60b45ba5681e265a62a3562e
JavikusRod <javierrod9191@gmail.com>
2019-11-04 14:25:00 -0500

    loading and toast component done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,17 @@ import { IonContent, IonPage, IonLoading } from '@ionic/react';
 import React, { useState, Fragment } from 'react';
 import NavLayaout from '../components/layout/nav/NavLayout.component';
 import MainLayaout from '../components/layout/main/MainLayaout.component';
+import Loading from '../components/layout/loading/Loading.component';
+import Toast from '../components/layout/toast/Toast.component';
 
 const Home: React.FC = () => {
   const [loading, setLoading] = useState(false);
+  const [showToast, setShowToast] = useState(false);
   return (
     <IonPage>
       <NavLayaout />
-      {loading ? <IonLoading isOpen={loading} message={'Loading...'} /> : null}
+      {loading ? <Loading loading={loading} /> : null}
+      {showToast ? <Toast showToast={showToast} /> : null}
       <Fragment>
         <IonContent className='ion-padding'>
           <MainLayaout />
