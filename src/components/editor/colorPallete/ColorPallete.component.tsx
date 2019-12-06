e6b9be5b2ad757e8b47fb1a1d90126ba381f9194
Cloode Repo <clooderepo814@gmail.com>
2019-12-06 12:44:52 -0600

    color picker

--- src/components/editor/colorPallete/ColorPallete.component.tsx
+++ src/components/editor/colorPallete/ColorPallete.component.tsx
@@ -3,17 +3,22 @@ import React, { Fragment, useContext } from 'react';
 import GeneralContext from '../../../context/global/general.context';
 import { IColor } from '../../../context/interfaces';
 import ColorPalleteItem from './ColorPalleteItem.component';
+import EditorContext from '../../../context/editor/editor.context';
+const { SwatchesPicker } = require('react-color');
 
 const ColorPallete: React.FC = () => {
   const { colors } = useContext(GeneralContext);
+  const { setColor } = useContext(EditorContext);
+
+  const handleSetColor = (color: any) => {
+    setColor(color.hex);
+  };
 
   return (
     <Fragment>
-      <div style={{ border: 'solid 2px black' }}>
+      <div style={{ border: 'solid 2px black', width:"450px" }}>
         <IonRow>
-          {(colors as IColor[]).map((color: IColor) => (
-            <ColorPalleteItem key={color.code} color={color} />
-          ))}
+          <SwatchesPicker width="450px" onChange={handleSetColor} />
         </IonRow>
       </div>
     </Fragment>
